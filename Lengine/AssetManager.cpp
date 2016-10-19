d7108d3d1c9929dcae36fff347cc0f75c29a31f4
Luke Roche <lr98lr@aol.com>
2016-10-18 21:55:15 -0400

    Update AssetManager.cpp

--- Lengine/AssetManager.cpp
+++ Lengine/AssetManager.cpp
@@ -2,7 +2,7 @@
 
 Lengine::TextureCache Lengine::AssetManager::_textureCache;
 
-
+//used as a wrapper for the actual finding algo from tecture cache
 Lengine::GLTexture Lengine::AssetManager::getTexture(std::string texturePath){
 	return _textureCache.getTexture(texturePath);
 }
