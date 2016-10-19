2a886903138738a0b6f3718e4e14546575d29517
Luke Roche <lr98lr@aol.com>
2016-10-18 21:53:49 -0400

    comments updated

--- Lengine/InputListener.cpp
+++ Lengine/InputListener.cpp
@@ -7,15 +7,17 @@ namespace Lengine{
 	}
 
 	
-
+	//sends info from event to map
 	void InputListener::keyPressed(unsigned int keyID){
 		_keyMap[keyID] = true;
 	}
-
+	
+	//nullifies the key pressed in previous frame
 	void InputListener::keyReleased(unsigned int keyID){
 		_keyMap[keyID] = false;
 	}
-
+	
+	//returns the value of the keymap
 	bool InputListener::isKeyPressed(unsigned int keyID){
 		auto it = _keyMap.find(keyID);
 		if(it != _keyMap.end())
