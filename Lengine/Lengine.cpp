68d8df8b987e24cbab69b1ccc2dc6fe0a0a30423
Luke Roche <lr98lr@aol.com>
2016-10-20 01:14:46 -0400

    Update Lengine.cpp

--- Lengine/Lengine.cpp
+++ Lengine/Lengine.cpp
@@ -4,10 +4,13 @@
 
 namespace Lengine{
 
+	//ititializes externals
 	int init(){
-
+	
+		//init sdl2
 		SDL_Init(SDL_INIT_EVERYTHING);
-
+	
+		//use double buffer
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
 		return 0;
